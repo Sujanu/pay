91e5f18e79fa039d4a43b5d50dacf8173b4bca19
Sujanu Giri <gireee57@gmail.com>
2024-07-07 21:59:09 +0545

    done

--- khalti_payment_app/lib/homepage.dart
+++ khalti_payment_app/lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -8,18 +9,68 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepagetState extends State<Homepage> {
-  String refrenceId="":
+  String refrenceId = "";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar:AppBar(title:const Text("khalti payment")),
-    body: Center(child: Column(
+    return Scaffold(
+      appBar: AppBar(title: const Text("khalti payment")),
+      body: Center(
+          child: Column(
         mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        ElevatedButton(onPressed: (){}, child: const Text("Pay with Khalti"))
-      ],
-    )),
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                payWithKhaltiInApp();
+              },
+              child: const Text("Pay with Khalti")),
+          Text(refrenceId)
+        ],
+      )),
     );
+  }
+
+  payWithKhaltiInApp() {
+    KhaltiScope.of(context).pay(
+        config: PaymentConfig(
+            amount: 1000,
+            productIdentity: "product id ",
+            productName: "product Name"),
+        preferences: [
+          PaymentPreference.khalti,
+        ],
+        onSuccess: onSuccess,
+        onFailure: onFailure,
+        onCancel: onCancel);
+  }
+
+  void onSuccess(PaymentSuccessModel success) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Payment successful"),
+            actions: [
+              SimpleDialogOption(
+                child: const Text("ok"),
+                onPressed: () {
+                  setState(() {
+                    refrenceId = success.idx;
+                  });
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  void onFailure(PaymentFailureModel faliure) {
+    debugPrint(faliure.toString());
+  }
+
+  void onCancel() {
+    debugPrint("cancelled");
   }
 }
--- khalti_payment_app/lib/main.dart
+++ khalti_payment_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 import './homepage.dart';
 
 void main() {
@@ -8,32 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Homepage()
+    return KhaltiScope(
+      publicKey: 'test_public_key_f9b63153748a4647b27862008a025378',
+      enabledDebugging: true,
+      
+      builder: (context, navKey) {
+        return MaterialApp(
+          title: 'Khalti Payment',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const Homepage(),
+          navigatorKey: navKey,
+          localizationsDelegates: const [KhaltiLocalizations.delegate],
+        );
+      },);
   }
 }
-
-
